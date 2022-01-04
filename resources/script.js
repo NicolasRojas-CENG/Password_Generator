99cfce963d6c700ad20d3389649cf7100c6ce499
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-01-03 23:14:50 -0500

    Added the logic for the upper case selector.

--- resources/script.js
+++ resources/script.js
@@ -8,14 +8,13 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   //var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  password = "hello";
   passwordText.value = password;
 
 }
 
 function main() {
   window.alert("Please select the criteria for your password remember that is must include atleast one of the options.");
-  window.prompt("Would you like UPPER CASE letters? y/n");
+  passwordCriteria.updateUpper();//window.prompt("Would you like UPPER CASE letters? y/n");
   window.prompt("Would you like lower case letters? y/n");
   window.prompt("Would you like numbers? y/n");
   window.prompt("Would you like special characters? (-?!/\) y/n");
@@ -24,3 +23,50 @@ function main() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", main);
+//Player's attributes.
+var passwordCriteria = {
+  upper : "",
+  updateUpper: function() {
+    var placeholder = window.prompt("Would you like UPPER CASE letters? y/n");
+    placeholder = placeholder.toLocaleLowerCase();
+    if (placeholder === "y" || placeholder === "yes") {
+      this.upper = 'yes';
+      console.log(this.upper);
+    }
+    else if (placeholder === "n" || placeholder === "no") {
+      this.upper = 'no';
+      console.log(this.upper);
+    } else {
+      this.updateUpper();
+    }
+  },
+  lower:"",
+  numbers:"",
+  special:"",
+  lenght:"",
+  reset: function() {
+      this.health = 100;
+      this.money = 10;
+      this.attack = 13;
+    },
+    refillHealth: function() {
+      if (this.money >= 7) {
+        window.alert("Refilling player's health by 30 for 9 dollars.");
+        this.health += 30;
+        this.money -= 9;
+      } 
+      else {
+        window.alert("You don't have enough money for a refill!");
+      }
+    },
+    upgradeAttack: function() {
+      if (this.money >= 7) {
+        window.alert("Upgrading player's attack by 8 for 8 dollars.");
+        this.attack += 8;
+        this.money -= 8;
+      } 
+      else {
+        window.alert("You don't have enough money for an upgrade!");
+      }
+    }
+};
