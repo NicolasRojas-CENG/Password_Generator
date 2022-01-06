5c9b8c4ca990e27b97ce034b908804a53c3d07e1
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-01-05 21:40:44 -0500

    Added pass-gen logic.

--- resources/script.js
+++ resources/script.js
@@ -1,40 +1,21 @@
-// Assignment code here
+const upperList = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+const lowerList = "abcdefghijklmnñopqrstuvwxyz";
+const numbersList = "0123456789";
+const symbolsList = "!@#$%^&*()_+=";
+var password;
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  //var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-}
-
-function main() {
-  window.alert("Please select the criteria for your password remember that is must include atleast one of the options.");
-  passwordCriteria.updateUpper();//window.prompt("Would you like UPPER CASE letters? y/n");
-  passwordCriteria.updateLower();//window.prompt("Would you like lower case letters? y/n");
-  passwordCriteria.updateNumbers();//window.prompt("Would you like numbers? y/n");
-  passwordCriteria.updateSpecial();//window.prompt("Would you like special characters? (-?!/\) y/n");
-  passwordCriteria.updateLength();//window.prompt("How many characters would you like in your password? 8-128");
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", main);
-//Player's attributes.
 var passwordCriteria = {
   upper : "",
   updateUpper: function() {
     var placeholder = window.prompt("Would you like UPPER CASE letters? y/n");
+    if (placeholder == null) {
+      throw new Error ("You have canceled password generation.");
+    }
     placeholder = placeholder.toLocaleLowerCase();
     if (placeholder === "y" || placeholder === "yes") {
       this.upper = 'yes';
-      console.log(this.upper);
     } else if (placeholder === "n" || placeholder === "no") {
       this.upper = 'no';
-      console.log(this.upper);
     } else {
       this.updateUpper();
     }
@@ -42,13 +23,14 @@ var passwordCriteria = {
   lower:"",
   updateLower: function() {
     var placeholder = window.prompt("Would you like lower case letters? y/n");
+    if (placeholder == null) {
+      throw new Error ("You have canceled password generation.");
+    }
     placeholder = placeholder.toLocaleLowerCase();
     if (placeholder === "y" || placeholder === "yes") {
       this.lower = 'yes';
-      console.log(this.lower);
     } else if (placeholder === "n" || placeholder === "no") {
       this.lower = 'no';
-      console.log(this.lower);
     } else {
       this.updateLower();
     }
@@ -56,13 +38,14 @@ var passwordCriteria = {
   numbers:"",
   updateNumbers: function() {
     var placeholder = window.prompt("Would you like numbers? y/n");
+    if (placeholder == null) {
+      throw new Error ("You have canceled password generation.");
+    }
     placeholder = placeholder.toLocaleLowerCase();
     if (placeholder === "y" || placeholder === "yes") {
       this.numbers = 'yes';
-      console.log(this.numbers);
     } else if (placeholder === "n" || placeholder === "no") {
       this.numbers = 'no';
-      console.log(this.numbers);
     } else {
       this.updateNumbers();
     }
@@ -70,13 +53,14 @@ var passwordCriteria = {
   special:"",
   updateSpecial: function() {
     var placeholder = window.prompt("Would you like special characters? (-?!/\) y/n");
+    if (placeholder == null) {
+      throw new Error ("You have canceled password generation.");
+    }
     placeholder = placeholder.toLocaleLowerCase();
     if (placeholder === "y" || placeholder === "yes") {
       this.special = 'yes';
-      console.log(this.special);
     } else if (placeholder === "n" || placeholder === "no") {
       this.special = 'no';
-      console.log(this.special);
     } else {
       this.updateSpecial();
     }
@@ -84,12 +68,95 @@ var passwordCriteria = {
   length:"",
   updateLength: function() {
     var placeholder = window.prompt("How many characters would you like in your password? 8-128");
+    if (placeholder == null) {
+      throw new Error ("You have canceled password generation.");
+    }
     placeholder = parseInt(placeholder);
     if (placeholder >= 8 && placeholder <= 128) {
       this.length = placeholder;
-      console.log(this.length);
     } else {
       this.updateLength();
     }
   },
+  password:"",
+  reset: function () {
+    this.password = "";
+  },
 };
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function main() {
+  window.alert("Please select the criteria for your password. Remember that is must include at least one of the options.");
+  gatherAnswers();
+  verifyAnswers();
+  passwordGen();
+  writePassword();
+  passwordCriteria.reset();
+}
+
+function gatherAnswers(){
+  passwordCriteria.updateUpper();
+  passwordCriteria.updateLower();
+  passwordCriteria.updateNumbers();
+  passwordCriteria.updateSpecial();
+  passwordCriteria.updateLength();
+}
+
+function verifyAnswers(){
+  if (passwordCriteria.upper === "no" && passwordCriteria.lower === "no" && passwordCriteria.numbers === "no" && passwordCriteria.special === "no"){
+    window.alert("Please select an option of characters as yes for the password to generate.");
+    gatherAnswers();
+  }
+}
+
+function passwordGen() {
+  var i = 0
+  while  (i < passwordCriteria.length) {
+    var selector = verifyCharSelector();
+    caseBranch = Math.floor(Math.random() * selector.length);
+    switch (selector.charAt(caseBranch)){
+      case "0":
+         var char = Math.floor(Math.random()* upperList.length);
+         passwordCriteria.password = passwordCriteria.password.concat(upperList.charAt(char));
+        break;
+      case "1":
+        var char = Math.floor(Math.random()* lowerList.length);
+         passwordCriteria.password = passwordCriteria.password.concat(lowerList.charAt(char));
+        break;
+      case "2":
+        var char = Math.floor(Math.random()* numbersList.length);
+         passwordCriteria.password = passwordCriteria.password.concat(numbersList.charAt(char));
+        break;
+      case "3":
+        var char = Math.floor(Math.random()* symbolsList.length);
+         passwordCriteria.password = passwordCriteria.password.concat(symbolsList.charAt(char));
+        break;
+    }
+    i++;
+  }
+}
+
+function verifyCharSelector(selector, comparator){
+  var caseSelectors = "";
+  if (passwordCriteria.upper == "yes"){
+    caseSelectors = caseSelectors.concat("0");
+  }
+  if (passwordCriteria.lower == "yes"){
+    caseSelectors = caseSelectors.concat("1");
+  }
+  if (passwordCriteria.numbers == "yes"){
+    caseSelectors = caseSelectors.concat("2");
+  }
+  if (passwordCriteria.special == "yes"){
+    caseSelectors = caseSelectors.concat("3");
+  }
+  return caseSelectors;
+}
+
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+  passwordText.value = passwordCriteria.password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", main);
